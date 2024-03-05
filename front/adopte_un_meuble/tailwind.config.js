c3fc4495b48b67e822575caf90c2bb74438da545
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-03-05 15:29:34 +0100

    fixed Tailwind

--- front/adopte_un_meuble/tailwind.config.js
+++ front/adopte_un_meuble/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightMode: {
+          background: "#FDFDFC",
+          text: "#21201C",
+          secondarytext: "#63635E",
+        },
+        darkMode: {
+          background: "#191918",
+          text: "#EEEEEC",
+          secondarytext: "#B5B3AD",
+        },
+      },
+    },
   },
   plugins: [],
 };
