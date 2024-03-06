6956d8ee16cd2898a9a9209edf3e92ec28fd3d9b
realalexis <alexismoulliere44@gmail.com>
2024-03-06 15:52:22 +0100

    change port heroku

--- back/src/app.js
+++ back/src/app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const publicationRoute = require("./routes/publicationRoute");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //Middleware pour parser le JSON et utiliser le fichier JSON
 app.use(express.json());
