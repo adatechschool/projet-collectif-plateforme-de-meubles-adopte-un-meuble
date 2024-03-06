2af2ec1e4e785bc701dc88ef271ad126db286f79
realalexis <alexismoulliere44@gmail.com>
2024-03-06 15:27:58 +0100

    heroku test

--- back/src/app.js
+++ back/src/app.js
@@ -2,17 +2,37 @@
 
 //Récupérer express et la fonction publicationRoute
 const express = require("express");
-
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const publicationRoute = require("./routes/publicationRoute");
 
 const app = express();
 const port = 3000;
+const API_SERVICE_URL = "http://jsonplaceholder.typicode.com";
 
 //Middleware pour parser le JSON et utiliser le fichier JSON
 app.use(express.json());
 
 app.use("/api/publication", publicationRoute);
 
+app.use("", (req, res, next) => {
+  if (req.headers.authorization) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
+app.use(
+  "/json_placeholder",
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: { [`^/json_placeholder`]: "" },
+  })
+);
+
+app.get("/info", (req, res, next) => {
+  res.send("Hello World!");
+});
 
 //Lancer le serveur
 app.listen(port, () => {
--- back/src/models/publicationModel.js
+++ back/src/models/publicationModel.js
@@ -16,52 +16,64 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 //Requête pour obtenir toutes les publications de la BDD :
 const getPublication = async (req, res) => {
-    const { data, error } = await supabase.from("Publications").select("*");
-    if (error) throw error;
-    return data;
+  const { data, error } = await supabase.from("Publications").select("*");
+  if (error) throw error;
+  return data;
 };
 
 //Requête pour obtenir une publication à partir de son id :
 const getPublicationById = async (req, res) => {
-    const { data, error } = await supabase.from("Publications").select("*").eq("id", req.params.id);
-    if (error) throw error;
-    return data;
+  const { data, error } = await supabase
+    .from("Publications")
+    .select("*")
+    .eq("id", req.params.id);
+  if (error) throw error;
+  return data;
 };
 
 // front doit renvoyer l'id de couleur, l'id de type et le prix
 // permet de filtrer les publications
 const filterPublication = async (req, res) => {
-    let query = supabase.from("Publications").select("*, Couleur!inner(id, couleur), Type!inner(id, type)");
-    const type = req.query.type;
-    const couleur = req.query.couleur;
-    const minPrice = req.query.minPrice;
-    const maxPrice = req.query.maxPrice;
+  let query = supabase
+    .from("Publications")
+    .select("*, Couleur!inner(id, couleur), Type!inner(id, type)");
+  const type = req.query.type;
+  const couleur = req.query.couleur;
+  const minPrice = req.query.minPrice;
+  const maxPrice = req.query.maxPrice;
 
-    if (type) {
-        query = query.eq("Type.type", type);
-    }
-    if (couleur) {
-        query = query.eq("Couleur.couleur", couleur);
-    }
-    if (minPrice) {
-        query = query.gte("prix", minPrice);
-    }
-    if (maxPrice) {
-        query = query.lte("prix", maxPrice);
-    }
+  if (type) {
+    query = query.eq("Type.type", type);
+  }
+  if (couleur) {
+    query = query.eq("Couleur.couleur", couleur);
+  }
+  if (minPrice) {
+    query = query.gte("prix", minPrice);
+  }
+  if (maxPrice) {
+    query = query.lte("prix", maxPrice);
+  }
 
-    const { data, error } = await query;
-    if (error) {
-        throw error;
-    }
-    return data;
+  const { data, error } = await query;
+  if (error) {
+    throw error;
+  }
+  return data;
 };
 
 const getEssentials = async (req, res) => {
-    const { data, error } = await supabase.from("Publications").select("titre, photos, prix");
-    if (error) throw error;
-    return data;
+  const { data, error } = await supabase
+    .from("Publications")
+    .select("titre, photos, prix");
+  if (error) throw error;
+  return data;
 };
 
 //Exporter les fonctions pour pouvoir les utiliser dans le fichier publicationControllers.js
-module.exports = { getPublication, getPublicationById, filterPublication, getEssentials };
+module.exports = {
+  getPublication,
+  getPublicationById,
+  filterPublication,
+  getEssentials,
+};
