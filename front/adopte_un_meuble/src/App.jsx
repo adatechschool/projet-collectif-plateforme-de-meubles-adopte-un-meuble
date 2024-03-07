c7e0c2596304c4d580458a817a72b227f696d47b
Amy <yambeddm@gmail.com>
2024-03-07 09:48:40 +0100

    productcard finie

--- front/adopte_un_meuble/src/App.jsx
+++ front/adopte_un_meuble/src/App.jsx
@@ -2,50 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import ProductCard from "./Components/ProductCard.jsx"
+import ProductCard from "./Components/ProductCard";
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="cards_section">
-        <ProductCard
-          image='src/assets/cat(1).jpg'
-          title='Baby Cat'
-          price='2$'
-          divers='this is a cat'
-          seller='Amy'
-        />
-        <ProductCard
-          image='src/assets/cat(2).jpg'
-          title='Unhappy Cat'
-          price='5$'
-          divers='this is a cat'
-          seller='Ames'
-        />
-        <ProductCard
-          image='src/assets/cat(3).jpg'
-          title='Anxious Cat'
-          price='3$'
-          divers='this is a cat'
-          seller='Anna'
-        />
-        <ProductCard
-          image='src/assets/cat(4).jpg'
-          title='Trying his best Cat'
-          price='1 candy'
-          divers='this is a cat'
-          seller='Marie'
-        />
-        <ProductCard
-          image='src/assets/cat(5).jpg'
-          title='gen Z Cat'
-          price='for free'
-          divers='this is a cat'
-          seller='Yuna'
-        />
-      </div>
+
     </>
   );
 }
--- front/adopte_un_meuble/src/Components/ProductCard.jsx
+++ front/adopte_un_meuble/src/Components/ProductCard.jsx
@@ -1,26 +1,17 @@
 import React from 'react'
-import './ProductCard.css'
 
 function ProductCard(props) {
     return (
-        // <div className="flex p-[0.625rem] flex-col items-start gap-[0.625rem] self-stretch">
         <div className="w-80 h-[443px] p-2.5 flex-col justify-start items-start gap-2.5 inline-flex">
-        {/* </div><div className="flex h-[18.75rem] flex-col justify-center items-start self-stretch"> */}
         <div className="self-stretch h-[300px] flex-col justify-center items-start flex">
-            {/* <img className="w-[36rem] h-[24.0625rem] shrink-0" src={props.image} /> */}
             <img className="w-[576px] h-[385px]" src={props.image} />
         </div>
-        {/* <div className="flex flex-col items-start gap-[0.3125rem] self-stretch"> */}
         <div className="self-stretch h-[113px] flex-col justify-start items-start gap-[5px] flex">
-            {/* <div className="text-stone-900 font-['Inter'] text-[1.875rem] not-italic font-bold leading-normal">{props.title}</div> */}
             <div className="text-stone-900 text-3xl font-bold font-['Inter']">{props.title}</div>
-            {/* <div className="flex flex-col items-start"> */}
             <div className="flex-col justify-start items-start flex">
-                {/* <div className="flex justify-end items-start gap-[0.625rem]">{props.price}</div> */}
                 <div className="text-black text-xl font-bold font-['Inter']">{props.price}</div>
             </div>
             <div className="text-stone-900 text-xl font-medium font-['Inter']">{props.divers}</div>
-            {/* <div className="text-stone-900 font-['Inter'] text-[1.25rem] not-italic font-medium leading-normal">{props.seller}</div> */}
             <div className="text-stone-900 text-xl font-medium font-['Inter']">{props.seller}</div>
         </div>
     </div>     
