f67fe9748a8d98ebfc84c2850d5cce33154de626
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-03-09 23:21:16 +0100

    carousel layout is complete

--- front/adopte_un_meuble/src/App.jsx
+++ front/adopte_un_meuble/src/App.jsx
@@ -23,6 +23,49 @@ const images = [
   "./public/lights_1.png",
 ];
 
+const articles = [
+  {
+    id: 1,
+    name: "dofa douilletHavre",
+    type: "canapé",
+    color: "vert",
+    state: "bon état",
+    photo: "./public/cabinet_1.png",
+  },
+  {
+    id: 2,
+    name: "Sofa Supreme",
+    type: "canapé",
+    color: "bleu",
+    state: "excellent état",
+    photo: "./public/chair_1.png",
+  },
+  {
+    id: 3,
+    name: "Elegant Coffee Table",
+    type: "table basse",
+    color: "marron",
+    state: "comme neuf",
+    photo: "./public/couch_1.png",
+  },
+  {
+    id: 4,
+    name: "Vintage Armchair",
+    type: "fauteuil",
+    color: "rouge",
+    state: "état vintage",
+    photo: "./public/couch_2.png",
+  },
+  {
+    id: 5,
+    name: "lampe",
+    type: "luminaire",
+    color: "blanc",
+    state: "bon état",
+    photo: "./public/lights_1.png",
+  },
+];
+
 function App() {
   return (
     <div className="flex w-screen h-screen flex-col">
@@ -100,12 +143,48 @@ function App() {
               {images.map((imageUrl, index) => (
                 <CarouselItem className="pt-1 basis-1/3" key={index}>
                   <div className="py-[5vh]">
-                    <div className="flex aspect-square items-center w-[60vh]">
+                    {/* <div className="flex flex-col justify-between items-start aspect-square w-[60vh] bg-red-500 p-[0.625rem] ">
                       <img
                         className="max-h-full max-w-none h-full"
                         src={imageUrl}
                         alt={`Image ${index + 1}`}
                       />
+                      <div className="flex flex-col items-start ">
+                        <h2 className="text-2xl text-lightMode-text font-bold bg-lightMode-background py-1 px-2.5">
+                          Divan Reconditionné Chic
+                        </h2>
+                        <h3 className="text-lg text-lightMode-text font-medium bg-lightMode-background py-1 px-2.5">
+                          comme neuf
+                        </h3>
+                      </div>
+                      <div className="flex items-start">
+                        <h3 className="text-lg text-lightMode-text font-medium bg-lightMode-background py-1 px-2.5">
+                          €250
+                        </h3>
+                      </div>
+                    </div> */}
+
+                    <div className="flex flex-col justify-between items-start aspect-square w-[60vh] bg-red-500 p-[0.625rem] relative">
+                      <div className="absolute inset-0">
+                        <img
+                          className="max-h-full max-w-none h-full w-full object-cover"
+                          src={imageUrl}
+                          alt={`Image ${index + 1}`}
+                        />
+                      </div>
+                      <div className="flex flex-col items-start relative z-10">
+                        <h2 className="text-2xl text-lightMode-text font-bold bg-lightMode-background py-1 px-2.5">
+                          Divan Reconditionné Chic
+                        </h2>
+                        <h3 className="text-lg text-lightMode-text font-medium bg-lightMode-background py-1 px-2.5">
+                          comme neuf
+                        </h3>
+                      </div>
+                      <div className="flex items-start relative z-10">
+                        <h3 className="text-lg text-lightMode-text font-medium bg-lightMode-background py-1 px-2.5">
+                          €250
+                        </h3>
+                      </div>
                     </div>
                   </div>
                 </CarouselItem>
