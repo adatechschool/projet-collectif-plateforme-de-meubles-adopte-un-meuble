46b7722d5641d29893d185eb2d788580dd970884
Fabienne-Lis <fabienne.lis@gmail.com>
2024-03-11 14:59:51 +0100

    création de la fonction pour récupérer le fetch

--- front/adopte_un_meuble/src/components/ShoppingCartItem.jsx
+++ front/adopte_un_meuble/src/components/ShoppingCartItem.jsx
@@ -4,14 +4,47 @@ function ShoppingCartItem(props) {
   return (
     <div className="w-full flex flex-col items-start gap-y-1">
       <h3 className="text-lightMode-text text-3xl font-bold">
-        Canapé Vintage Rétro
+        {props.titre}
       </h3>
-      <h4 className="text-lightMode-secondarytext text-2xl font-bold">€250</h4>
+      <h4 className="text-lightMode-secondarytext text-2xl font-bold">{props.prix}</h4>
       <h4 className="text-lightMode-secondarytext text-2xl font-bold underline">
-        supprimer
+        {props.supprimer}
       </h4>
     </div>
   );
 }
 
 export default ShoppingCartItem;
+
+//récupération de la base de données
+//faire un if = id prendre le titre de id
+
+async function buyPublication(props) {
+  const rep = await fetch("api/publication");
+  const publication = await rep.json();
+  console.log("publication", publication);
+  const titre = publication[1].titre;
+  console.log("titre", titre)
+}
+
+buyPublication()
+
+//async function getFetchCart() {
+  //const reponse = await fetch("http://localhost:3000/api/publication/panier/4");
+  //const panier = await reponse.json();
+  //console.log("panier", panier);
+  //const publicationTitle = await publication[1].titre
+  //console.log("publicationTitle", publicationTitle)
+//}
+
+//getFetchCart()
+
+
+
+//fonction qui change le panier
+//function getCart() {
+  //const ex = ShoppingCartItem(props)
+  //console.log(ex, props.title)
+//}
+
+//getCart()
