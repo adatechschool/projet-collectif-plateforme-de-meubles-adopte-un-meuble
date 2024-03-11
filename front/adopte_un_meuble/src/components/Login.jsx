3987517a676e43fa3e0db5882229e16d0cac1eb5
realalexis <alexismoulliere44@gmail.com>
2024-03-11 12:18:00 +0100

    test session

--- front/adopte_un_meuble/src/components/Login.jsx
+++ front/adopte_un_meuble/src/components/Login.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpmcm93a21od2huaG15end4bGV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk1NTg5NjUsImV4cCI6MjAyNTEzNDk2NX0.6HS7PaiqkOQtN3JPNCCBAW2058bJNQoAuECWeurKlYM";
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  "https://zfrowkmhwhnhmyzwxlez.supabase.co",
+  supabaseKey
+);
 
 import { Button } from "@/components/ui/button";
 import {
@@ -14,34 +23,41 @@ import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function Login() {
-  // useEffect(() => {}, []);
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log("heu login");
     fetch("http://localhost:3000/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({}),
+      body: JSON.stringify({ email, password }),
     })
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
       });
-    console.log("login");
+    const { data, error } = await supabase.auth.getSession();
+    if (error) {
+      console.log("error", error);
+    }
+    console.log("login", data);
   };
 
   const handleRegister = (e) => {
-    const nom = e.target.nom.value;
-    const prenom = e.target.prenom.value;
-    const email = e.target.email.value;
-    const mdp = e.target.mdp.value;
+    e.preventDefault();
+    console.log(nom, prenom, email, password);
     fetch("http://localhost:3000/api/auth/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, mdp, nom, prenom }),
+      body: JSON.stringify({ email, password, nom, prenom }),
     })
       .then((res) => res.json())
       .then((data) => {
@@ -72,14 +88,22 @@ function Login() {
             <form onSubmit={handleLogin}>
               <CardContent className="space-y-2">
                 <div className="space-y-1">
-                  <Input id="name" placeHolder="adresse mail" />
+                  <Input
+                    id="loginMail"
+                    placeHolder="adresse mail"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
                 <div className="space-y-1">
-                  <Input id="username" placeHolder="votre mot de passe" />
+                  <Input
+                    id="loginMdp"
+                    placeHolder="votre mot de passe"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </div>
               </CardContent>
               <CardFooter>
-                <Button>Connection</Button>
+                <Button type="submit">Connection</Button>
               </CardFooter>
             </form>
           </Card>
@@ -95,20 +119,40 @@ function Login() {
             <form onSubmit={handleRegister}>
               <CardContent className="space-y-2">
                 <div className="space-y-1">
-                  <Input id="nom" type="username" placeHolder="Nom" />
+                  <Input
+                    id="nom"
+                    type="username"
+                    placeHolder="Nom"
+                    onChange={(e) => setNom(e.target.value)}
+                  />
                 </div>
                 <div className="space-y-1">
-                  <Input id="prenom" type="username" placeHolder="Prénom" />
+                  <Input
+                    id="prenom"
+                    type="username"
+                    placeHolder="Prénom"
+                    onChange={(e) => setPrenom(e.target.value)}
+                  />
                 </div>
                 <div className="space-y-1">
-                  <Input id="email" type="mail" placeHolder="adresse mail" />
+                  <Input
+                    id="email"
+                    type="mail"
+                    placeHolder="adresse mail"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
                 <div className="space-y-1">
-                  <Input id="mdp" type="password" placeHolder="mot de passe" />
+                  <Input
+                    id="mdp"
+                    type="password"
+                    placeHolder="mot de passe"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </div>
               </CardContent>
               <CardFooter>
-                <Button>Créer votre compte</Button>
+                <Button type="submit">Créer votre compte</Button>
               </CardFooter>
             </form>
           </Card>
