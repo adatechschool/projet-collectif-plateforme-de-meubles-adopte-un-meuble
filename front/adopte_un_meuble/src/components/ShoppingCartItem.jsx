ca33fe9106bd14b7e171604105fe49b6e0ba6581
Fabienne-Lis <fabienne.lis@gmail.com>
2024-03-12 11:31:52 +0100

    correction boucle + sélection article par id

--- front/adopte_un_meuble/src/components/ShoppingCartItem.jsx
+++ front/adopte_un_meuble/src/components/ShoppingCartItem.jsx
@@ -3,31 +3,43 @@ import React, { useEffect, useState } from "react";
 function ShoppingCartContainer() {
   const [publications, setPublications] = useState([]);
 
+  const idProduit = 3;
   useEffect(() => {
-    async function fetchPublications() {
+    async function fetchPublications(id) {
       try {
-        const response = await fetch("/api/publication");
+        const response = await fetch(`http://localhost:3000/api/publication/${id}`);
         if (!response.ok) {
           throw new Error("Erreur lors de la récupération des publications");
         }
         const data = await response.json();
+        console.log(data)
         setPublications(data);
       } catch (error) {
         console.error("Erreur:", error.message);
       }
     }
-    fetchPublications();
+    fetchPublications(idProduit);
   }, []);
 
   async function handleSupprimerPublication(id) {
-    console.log("Supprimer publication avec l'ID:", id);
-    // Ici, vous pouvez ajouter la logique pour supprimer la publication
+    try {
+      const response = await fetch(`/api/publication/deletePanier/${id}`, {
+        method: "DELETE"
+      });
+      if (!response.ok) {
+        throw new Error("Erreur lors de la suppression de la publication du panier");
+      }
+      console.log("Publication supprimée du panier avec l'ID:", id);
+      // Ajoutez ici la logique pour mettre à jour l'état des publications après la suppression
+    } catch (error) {
+      console.error("Erreur:", error.message);
+    }
   }
 
   return (
     <div>
       {publications.map((publication) => (
-        <div key={publication.id}  className="w-full flex flex-col items-start gap-y-1">
+        <div key={publication.id} className="w-full flex flex-col items-start gap-y-1">
           <h3 className="text-lightMode-text text-3xl font-bold">{publication.titre}</h3>
           <h4 className="text-lightMode-secondarytext text-2xl font-bold">{publication.prix}</h4>
           <button onClick={() => handleSupprimerPublication(publication.id)} className="text-lightMode-secondarytext text-2xl font-bold underline">
@@ -40,4 +52,3 @@ function ShoppingCartContainer() {
 }
 
 export default ShoppingCartContainer;
-
