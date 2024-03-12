809b3f0f2eca91d49aa9717d68054fdb7bcf1a2e
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-03-12 15:56:36 +0100

    little detail

--- front/adopte_un_meuble/src/components/Navbar.jsx
+++ front/adopte_un_meuble/src/components/Navbar.jsx
@@ -67,7 +67,7 @@ function Navbar() {
           <Sheet>
             <SheetTrigger>
               <svg
-                className="hidden sm:flex"
+                className="hidden sm:flex hover:rotate-12 transition-transform"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
                 height="19"
